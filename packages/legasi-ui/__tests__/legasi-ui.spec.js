b935cc29dc30ea1c4631260cd3f03b283f496f4f
Reinout Mechant <reinout.mechant@imprss.be>
2019-02-23 15:31:15 +0100

    Did some additional changes

--- packages/legasi-ui/__tests__/legasi-ui.spec.js
+++ packages/legasi-ui/__tests__/legasi-ui.spec.js
@@ -4,6 +4,8 @@ describe('state', () => {
 	it('legasi should start', () => {
 		var isOk = false;
 		if (legasi) isOk = true;
+		var result = legasi.test();
 		expect(isOk).toBe(true);
+		expect(result).toBe("yes");
 	})
 });
